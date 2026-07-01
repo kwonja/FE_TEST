18173be7f4471a84de63ad9beaa3aa07ab87c325
kwonja <snna58@naver.com>
2026-07-01 12:32:01 +0900

    일정 UI 라우트 연결 제거

--- app/(schedule)/calendar/page.tsx
+++
@@ -1,6 +0,0 @@
-import { CalendarScreen } from "@/features/schedule/components/calendar-screen";
-
-export default function CalendarPage() {
-  return <CalendarScreen />;
-}
-
--- app/(schedule)/table/page.tsx
+++
@@ -1,6 +0,0 @@
-import { TableScreen } from "@/features/schedule/components/table-screen";
-
-export default function TablePage() {
-  return <TableScreen />;
-}
-
